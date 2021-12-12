859c74783158364802c8014de2280002b2bbed48
4eshiiire <svetp93@gmail.com>
2021-12-12 18:47:50 +0200

    added my credentials

--- data/login.data.js
+++ data/login.data.js
@@ -1,3 +1,4 @@
 module.exports = {
-  leadCredentials: { email: "Kim5@gmail.com", password: "0_sZqFkQeMoXWAp" },
-};
+  leadCredentials: { email: 'Kim5@gmail.com', password: '0_sZqFkQeMoXWAp' },
+  leadCredentials: { email: 'svetp93@mail.ru', password: '1Qwerty!' },
+}
