8d29ea39f6ad710e7b6fa271f4f3edf24f7781d1
Venom207 <62240450+Venom207@users.noreply.github.com>
2021-12-12 19:49:13 +0300

    add credentials vit

--- data/login.data.js
+++ data/login.data.js
@@ -1,3 +1,7 @@
 module.exports = {
   leadCredentials: { email: "Kim5@gmail.com", password: "0_sZqFkQeMoXWAp" },
+  vitCredentials: {
+    email: "Lavern.Mayer71@yahoo.com",
+    password: "BCUhpWS2A_mxXJJ",
+  },
 };
