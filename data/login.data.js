c7ebc2d755e5a72c391ef86f68e47991bcb677a6
Anna Doronina <doronina.anna2021@gmail.com>
2021-12-12 20:19:27 +0200

    Adding doronina credentials

--- data/login.data.js
+++ data/login.data.js
@@ -1,3 +1,4 @@
 module.exports = {
   leadCredentials: { email: "Kim5@gmail.com", password: "0_sZqFkQeMoXWAp" },
+  doroninaCredentials: { email: "doroninaCredentials.anna2021@gmail.com", password: "Anna123456789*"}
 };
