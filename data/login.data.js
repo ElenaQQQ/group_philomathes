893b337ea976dddabd3aceb620542fadff1d6c39
AnastasiyaDavadovich <anastasiyadavidovichy@gmail.com>
2021-12-12 19:47:51 +0300

    Added Enot credentials

--- data/login.data.js
+++ data/login.data.js
@@ -1,3 +1,4 @@
 module.exports = {
   leadCredentials: { email: "Kim5@gmail.com", password: "0_sZqFkQeMoXWAp" },
+  NastyaCredentials: { email: "anastasiyadavidovichy@gmail.com", password: "Zxc15531553!" }
 };
