05ad23bbb31a96bda9c2bae81e96d28de284f636
Anna Doronina <doronina.anna2021@gmail.com>
2021-12-13 00:21:21 +0200

    again re-wrigth password

--- data/login.data.js
+++ data/login.data.js
@@ -1,6 +1,6 @@
 module.exports = {
   leadCredentials: { email: "Kim5@gmail.com", password: "0_sZqFkQeMoXWAp" },
-  doroninaCredentials: { email: "test4tet@i.ua", password: "qwerty12!"}
+  doroninaCredentials: { email: "test4tet@i.ua", password: "Anna12!"},
   vitCredentials: { email: "Lavern.Mayer71@yahoo.com", password: "BCUhpWS2A_mxXJJ" },
   olgaJSCredentials: { email: "ok17451@gmail.com", password: "ProjectJS" },
   artyomCredentials: { email: "muzzoid@bk.ru", password: "Qwe123Zxc^" },
